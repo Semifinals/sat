20addfb70e64e94c5b2753894f1214132b64bab8
Finley Sherwood <me@itsf.in>
2023-04-02 15:20:28 +1000

    Add .NET language support

---
+++ cs/Token.Tests.Unit/TokenTests.cs
@@ -0,0 +1,230 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace Semifinals.Utils.Tokens;
+
+[TestClass]
+public class TokenTests
+{
+    public static readonly string TestSecret = "secret";
+
+    [TestMethod]
+    public void Epoch()
+    {
+        // Arrange
+        long epoch = 1672531200;
+
+        // Act
+        long res = Token.Epoch;
+
+        // Assert
+        Assert.AreEqual(epoch, res);
+    }
+
+    [TestMethod]
+    public void Separator()
+    {
+        // Arrange
+        char separator = '.';
+
+        // Act
+        char res = Token.Separator;
+
+        // Assert
+        Assert.AreEqual(separator, res);
+    }
+
+    [TestMethod]
+    public void Generate()
+    {
+        // Arrange
+        string id = "test";
+
+        // Act
+        string res = Token.Generate(id, TestSecret);
+
+        // Assert
+        string resId = Token.GetId(res)!;
+        long resTimestamp = (long)Token.GetTimestamp(res)!;
+        string resSignature = Token.GetSignature(res)!;
+        Assert.AreEqual(id, resId);
+
+        string payload = Token.Payload(resId, resTimestamp);
+        string signature = Token.Sign(payload, TestSecret);
+        Assert.AreEqual(resSignature, signature);
+    }
+
+    [TestMethod]
+    [DataRow("id", (1672531200L + 1) * 1000, "aWQ=.MQ==")]
+    [DataRow("test", (1672531200L + 86400) * 1000, "dGVzdA==.ODY0MDA=")]
+    [DataRow("working", (1672531200L + 1234567890) * 1000, "d29ya2luZw==.MTIzNDU2Nzg5MA==")]
+    public void Payload(string id, long timestamp, string expected)
+    {
+        // Arrange
+
+        // Act
+        string res = Token.Payload(id, timestamp);
+
+        // Assert
+        Assert.AreEqual(expected, res);
+    }
+
+    [TestMethod]
+    [DataRow("aWQ=.MQ==", "PNvHPV1cdk47r68wzAugWGeHfjNZOa6Su+7qj67U8ok=")]
+    [DataRow("dGVzdA==.ODY0MDA=", "iNsbhu5s1rdoPT960fY0Bu7sQAaaP2ysD3RJS9DQUmg=")]
+    [DataRow("d29ya2luZw==.MTIzNDU2Nzg5MA==", "YHdIO9UBOvAO7cTrKLJdRvE9FVuGCCVvI6bCZvtCuWE=")]
+    public void Sign(string payload, string expected)
+    {
+        // Arrange
+
+        // Act
+        string res = Token.Sign(payload, TestSecret);
+
+        // Assert
+        Assert.AreEqual(expected, res);
+    }
+
+    [TestMethod]
+    [DataRow("aWQ=.MQ==.PNvHPV1cdk47r68wzAugWGeHfjNZOa6Su+7qj67U8ok=", true)]
+    [DataRow("dGVzdA==.ODY0MDA=.iNsbhu5s1rdoPT960fY0Bu7sQAaaP2ysD3RJS9DQUmg=", true)]
+    [DataRow("d29ya2luZw==.MTIzNDU2Nzg5MA==.YHdIO9UBOvAO7cTrKLJdRvE9FVuGCCVvI6bCZvtCuWE=", true)]
+    [DataRow(".invalid.invalid", false)]
+    [DataRow("invalid..invalid", false)]
+    [DataRow("..", false)]
+    [DataRow("", false)]
+    public void Validate(string token, bool expected)
+    {
+        // Arrange
+
+        // Act
+        bool res = Token.Validate(token);
+
+        // Assert
+        Assert.AreEqual(expected, res);
+    }
+
+    [TestMethod]
+    [DataRow("aWQ=.MQ==.PNvHPV1cdk47r68wzAugWGeHfjNZOa6Su+7qj67U8ok=", true)]
+    [DataRow("dGVzdA==.ODY0MDA=.iNsbhu5s1rdoPT960fY0Bu7sQAaaP2ysD3RJS9DQUmg=", true)]
+    [DataRow("d29ya2luZw==.MTIzNDU2Nzg5MA==.YHdIO9UBOvAO7cTrKLJdRvE9FVuGCCVvI6bCZvtCuWE=", true)]
+    [DataRow(".invalid.invalid", false)]
+    [DataRow("invalid..invalid", false)]
+    [DataRow("..", false)]
+    [DataRow("", false)]
+    public void Verify(string token, bool expected)
+    {
+        // Arrange
+
+        // Act
+        bool res = Token.Verify(token, TestSecret);
+
+        // Assert
+        Assert.AreEqual(expected, res);
+    }
+
+    [TestMethod]
+    [DataRow("MQ==.MQ==.MQ==", "1")]
+    [DataRow("ODY0MDA=.ODY0MDA=.ODY0MDA=", "86400")]
+    [DataRow("dGVzdA==.invalid", null)]
+    [DataRow("dGVzdA==", null)]
+    [DataRow("", null)]
+    public void GetId(string str, string? expected)
+    {
+        // Arrange
+
+        // Act
+        string? res = Token.GetId(str);
+
+        // Assert
+        Assert.AreEqual(expected, res);
+    }
+
+    [TestMethod]
+    [DataRow("ODY0MDA=.ODY0MDA=.ODY0MDA=", (1672531200L + 86400) * 1000)]
+    [DataRow("MQ==.MQ==.MQ==", (1672531200L + 1) * 1000)]
+    [DataRow("no.MQ==", null)]
+    [DataRow("fail", null)]
+    [DataRow("", null)]
+    public void GetTimestamp(string str, long? expected)
+    {
+        // Arrange
+
+        // Act
+        long? res = Token.GetTimestamp(str);
+
+        // Assert
+        Assert.AreEqual(expected, res);
+    }
+
+    [TestMethod]
+    [DataRow("ODY0MDA=.ODY0MDA=.valid", "valid")]
+    [DataRow("this.isnt", null)]
+    [DataRow("", null)]
+    public void GetSignature(string str, string? expected)
+    {
+        // Arrange
+
+        // Act
+        string? res = Token.GetSignature(str);
+
+        // Assert
+        Assert.AreEqual(expected, res);
+    }
+
+    [TestMethod]
+    [DataRow(new string[] { "this.has.dots", "this", "has", "dots" })]
+    [DataRow(new string[] { "this.also.has.dots", "this", "also", "has", "dots" })]
+    [DataRow(new string[] { "!@#.$%^", "!@#", "$%^" })]
+    [DataRow(new string[] { "nodots", "nodots" })]
+    [DataRow(new string[] { "..", "", "", "" })]
+    [DataRow(new string[] { "", "" })]
+    public void Split(string[] values)
+    {
+        // Arrange
+        string str = values[0];
+        string[] expected = values.Skip(1).ToArray();
+
+        // Act
+        string[] res = Token.Split(str);
+
+        // Assert
+        bool match = true;
+        for (int i = 0; i < res.Length; i++)
+            if (res[i] != expected[i])
+                match = false;
+
+        Assert.IsTrue(match);
+    }
+
+    [TestMethod]
+    [DataRow("decoded!", "ZGVjb2RlZCE=")]
+    [DataRow("abcdef123456", "YWJjZGVmMTIzNDU2")]
+    [DataRow("!@#$%^&*()_", "IUAjJCVeJiooKV8=")]
+    [DataRow("", "")]
+    public void ToBase64(string utf8, string expected)
+    {
+        // Arrange
+
+        // Act
+        string res = Token.ToBase64(utf8);
+
+        // Assert
+        Assert.AreEqual(expected, res);
+    }
+
+    [TestMethod]
+    [DataRow("ZGVjb2RlZCE=", "decoded!")]
+    [DataRow("YWJjZGVmMTIzNDU2", "abcdef123456")]
+    [DataRow("IUAjJCVeJiooKV8=", "!@#$%^&*()_")]
+    [DataRow("", "")]
+    public void ToUtf8(string base64, string expected)
+    {
+        // Arrange
+
+        // Act
+        string res = Token.ToUtf8(base64);
+
+        // Assert
+        Assert.AreEqual(expected, res);
+    }
+}
---
+++ cs/Token/Token.cs
@@ -0,0 +1,216 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Semifinals.Utils.Tokens;
+
+public class Token
+{
+    /// <summary>
+    /// The Semifinals Authentication Token epoch time (1st January 2023) in seconds.
+    /// </summary>
+    public static readonly long Epoch = ((DateTimeOffset)new DateTime(2023, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc)).ToUnixTimeSeconds();
+
+    /// <summary>
+    /// The separator used to partition the token.
+    /// </summary>
+    public static readonly char Separator = '.';
+
+    /// <summary>
+    /// Generate a token for the given ID.
+    /// </summary>
+    /// <param name="id">The ID for the token</param>
+    /// <param name="secret">The secret used for the sha256 algorithm</param>
+    /// <returns>The generated token</returns>
+    public static string Generate(string id, string secret)
+    {
+        long timestamp = ((DateTimeOffset)DateTime.UtcNow).ToUnixTimeMilliseconds();
+
+        string payload = Payload(id, timestamp);
+        string signature = Sign(payload, secret);
+
+        return payload + Separator + signature;
+    }
+
+    /// <summary>
+    /// Generate a payload using the given ID and timestamp.
+    /// </summary>
+    /// <param name="id">The ID for the token</param>
+    /// <param name="timestamp">The timestamp for the token</param>
+    /// <returns>The resulting payload</returns>
+    public static string Payload(string id, long timestamp)
+    {
+        string encodedId = ToBase64(id);
+        string encodedTimestamp = ToBase64((timestamp / 1000 - Epoch).ToString());
+
+        return encodedId + Separator + encodedTimestamp;
+    }
+
+    /// <summary>
+    /// Generate a signature for the given payload.
+    /// </summary>
+    /// <param name="payload">The payload to sign</param>
+    /// <param name="secret">The secret used for the sha256 algorithm</param>
+    /// <returns>The generated signature</returns>
+    public static string Sign(string payload, string secret)
+    {
+        byte[] payloadBytes = Encoding.UTF8.GetBytes(payload);
+
+        byte[] secretBytes = Encoding.UTF8.GetBytes(secret);
+
+        using HMACSHA256 hmac = new(secretBytes);
+        byte[] signatureBytes = hmac.ComputeHash(payloadBytes);
+
+        string signature = Convert.ToBase64String(signatureBytes);
+        return signature;
+    }
+
+    /// <summary>
+    /// Validate that the token looks correct in structure.
+    /// </summary>
+    /// <param name="token">The token to validate</param>
+    /// <returns>Whether or not the token is valid</returns>
+    public static bool Validate(string token)
+    {
+        string[] parts = Split(token);
+        if (parts.Length != 3)
+            return false;
+
+        string id = ToUtf8(parts[0]);
+        if (id.Length == 0)
+            return false;
+
+        string unparsedTimestamp = ToUtf8(parts[1]);
+        if (unparsedTimestamp.Length == 0)
+            return false;
+
+        if (!long.TryParse(unparsedTimestamp, out long _))
+            return false;
+
+        string signature = parts[2];
+        if (signature.Length == 0)
+            return false;
+
+        return true;
+    }
+
+    /// <summary>
+    /// Verify that the signature of the token is authentic.
+    /// </summary>
+    /// <param name="token">The full token</param>
+    /// <param name="secret">The secret used for the sha256 algorithm</param>
+    /// <returns>Whether or not the token was verified to be authentic</returns>
+    public static bool Verify(string token, string secret)
+    {
+        if (!Validate(token))
+            return false;
+
+        string? id = GetId(token);
+        long? timestamp = GetTimestamp(token);
+        string? signature = GetSignature(token);
+
+        if (id is null || timestamp is null || signature is null)
+            return false;
+
+        string payload = Payload(id, (long)timestamp);
+        string generatedSignature = Sign(payload, secret);
+
+        return signature == generatedSignature;
+    }
+
+    /// <summary>
+    /// Get the ID of the token.
+    /// </summary>
+    /// <param name="token">The full token</param>
+    /// <returns>The ID of the token</returns>
+    public static string? GetId(string token)
+    {
+        if (!Validate(token))
+            return null;
+
+        string[] parts = Split(token);
+        string id = ToUtf8(parts[0]);
+
+        return id;
+    }
+
+    /// <summary>
+    /// Get the timestamp of the token.
+    /// </summary>
+    /// <param name="token">The full token</param>
+    /// <returns>The Semifinals epoch timestamp of the token in seconds</returns>
+    public static long? GetTimestamp(string token)
+    {
+        if (!Validate(token))
+            return null;
+
+        string[] parts = Split(token);
+        
+        if (!long.TryParse(ToUtf8(parts[1]), out long timestamp))
+            return null;
+            
+
+        return (Epoch + timestamp) * 1000;
+    }
+
+    /// <summary>
+    /// Get the signature from a token.
+    /// </summary>
+    /// <param name="token">The full token</param>
+    /// <returns>The token's signature</returns>
+    public static string? GetSignature(string token)
+    {
+        if (!Validate(token))
+            return null;
+
+        string[] parts = Split(token);
+        string signature = parts[2];
+
+        return signature;
+    }
+
+    /// <summary>
+    /// Split a string into parts by the token separator.
+    /// </summary>
+    /// <param name="str">The string to split</param>
+    /// <returns>The string split into its parts</returns>
+    public static string[] Split(string str)
+    {
+        return str.Split(Separator);
+    }
+
+    /// <summary>
+    /// Convert a utf8 string into base64
+    /// </summary>
+    /// <param name="str">The utf8 string</param>
+    /// <returns>The string converted to base64</returns>
+    public static string ToBase64(string str)
+    {
+        try
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(str);
+            return Convert.ToBase64String(bytes);
+        }
+        catch (FormatException)
+        {
+            return "";
+        }
+    }
+
+    /// <summary>
+    /// Convert a base64 string into utf8.
+    /// </summary>
+    /// <param name="str">The base64 string</param>
+    /// <returns>The string converted to utf8</returns>
+    public static string ToUtf8(string str)
+    {
+        try
+        {
+            byte[] bytes = Convert.FromBase64String(str);
+            return Encoding.UTF8.GetString(bytes);
+        }
+        catch (FormatException)
+        {
+            return "";
+        }
+    }
+}
